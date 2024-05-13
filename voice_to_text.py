f24f24b09e29bc1aef2824471fe0696eed32a90c
arizmuajianisan <arizmuajianisan@gmail.com>
2024-05-13 19:47:16 +0700

    
    qqqPlease enter the commit message for your changes. Lines starting

--- voice_to_text.py
+++ voice_to_text.py
@@ -2,7 +2,7 @@ import vosk
 import pyaudio
 import json
 
-# Here I have downloaded this model to my PC, extracted the files 
+# Here I have downloaded this model to my PC, extracted the files
 # and saved it in local directory
 # Set the model path
 model_path = "model/vosk-model-small-ja-0.22"
@@ -10,21 +10,20 @@ model_path = "model/vosk-model-small-ja-0.22"
 # Initialize the model with model-path
 model = vosk.Model(model_path)
 
-#if you don't want to download the model, just mention "lang" argument 
-#in vosk.Model() and it will download the right  model, here the language is 
-#US-English
-#model = vosk.Model(lang="en-us")
+# if you don't want to download the model, just mention "lang" argument
+# in vosk.Model() and it will download the right  model, here the language is
+# US-English
+# model = vosk.Model(lang="en-us")
 
 # Create a recognizer
 rec = vosk.KaldiRecognizer(model, 16000)
 
 # Open the microphone stream
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16,
-                channels=1,
-                rate=16000,
-                input=True,
-                frames_per_buffer=8192)
+stream = p.open(
+    # format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192
+    format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024
+)
 
 # Specify the path for the output text file
 output_file_path = "recognized_text.txt"
@@ -34,16 +33,16 @@ with open(output_file_path, "a", encoding="utf-8") as output_file:
     print("Listening for speech. Say 'Terminate' to stop.")
     # Start streaming and recognize speech
     while True:
-        data = stream.read(4096)#read in chunks of 4096 bytes
-        if rec.AcceptWaveform(data):#accept waveform of input voice
+        data = stream.read(4096)  # read in chunks of 4096 bytes
+        if rec.AcceptWaveform(data):  # accept waveform of input voice
             # Parse the JSON result and get the recognized text
             result = json.loads(rec.Result())
-            recognized_text = result['text']
-            
+            recognized_text = result["text"]
+
             # Write recognized text to the file
             output_file.write(recognized_text + "\n")
             print(recognized_text)
-            
+
             # Check for the termination keyword
             if "日本" in recognized_text.lower():
                 print("Termination keyword detected. Stopping...")
